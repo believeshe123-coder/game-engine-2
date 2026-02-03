41cadcb5fcec413e35a608bacc2ef5323dd88998
believeshe123-coder <believeshe123@gmail.com>
2026-02-03 07:11:07 -0600

    Replace Table component per instructions

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import Table from './components/Table.jsx';
+
+const App = () => {
+  return (
+    <div className="app">
+      <header className="app__header">Tabletop MVP</header>
+      <main className="app__main">
+        <Table />
+      </main>
+    </div>
+  );
+};
+
+export default App;
